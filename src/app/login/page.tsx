2a7f874fc7de41222fa7acfaecfc3d0d1cb62527
akshayrx <hello.arxports@gmail.com>
2025-03-04 18:06:05 +0530

    folder structure boilerplate

---
+++ src/app/login/page.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage(){
+    return (
+        <div className="flex w-full items-center justify-center">
+            <h1 className="text-xl">Login Page</h1>
+        </div>
+    )
+}
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,7 @@
+export default function SignupPage(){
+    return(
+        <div className="flex justify-center">
+            <h1 className="text-xl">Signup Page</h1>
+        </div>
+    )
+}
