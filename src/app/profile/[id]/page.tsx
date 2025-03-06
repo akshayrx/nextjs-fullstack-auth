7e15aa50d3f7210f0580020a013eeee7465de184
akshayrx <hello.arxports@gmail.com>
2025-03-06 20:52:58 +0530

    resolving promise error

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,18 +1,19 @@
 interface Params {
     id: string;
-}
-
-export default async function UserProfile({params}: { params: Params }){
-    // await params
-    const userid = await params.id;
-
+  }
+  
+  export default async function UserProfile({ params }: { params: Params }) {
+    const userid = params.id; // No await needed
+  
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen py-2 gap-4">
-            <h1 className="text-xl">Profile Page</h1>
-            <hr />
-            <div className="flex flex-col justify-center items-start gap-2">
-                <p className="text-4xl">user profile: <span className="bg-orange-500 text-zinc-900">{userid}</span></p>
-            </div>
+      <div className="flex flex-col items-center justify-center min-h-screen py-2 gap-4">
+        <h1 className="text-xl">Profile Page</h1>
+        <hr />
+        <div className="flex flex-col justify-center items-start gap-2">
+          <p className="text-4xl">
+            user profile: <span className="bg-orange-500 text-zinc-900">{userid}</span>
+          </p>
         </div>
-    )
-}
+      </div>
+    );
+  }
